b2747a53ebaf8d29a8f53bf83df045787b355728
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2023-04-05 16:59:45 +0800

    init Diamond contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,75 @@
-import { ethers } from "hardhat";
+/* global ethers */
+/* eslint prefer-const: "off" */
+//@ts-nocheck
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const { getSelectors, FacetCutAction } = require('./libraries/diamond.ts')
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+async function deployDiamond () {
+  const accounts = await ethers.getSigners()
+  const contractOwner = accounts[0]
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Deploy DiamondInit
+  // DiamondInit provides a function that is called when the diamond is upgraded or deployed to initialize state variables
+  // Read about how the diamondCut function works in the EIP2535 Diamonds standard
+  const DiamondInit = await ethers.getContractFactory('DiamondInit')
+  const diamondInit = await DiamondInit.deploy()
+  await diamondInit.deployed()
+  console.log('DiamondInit deployed:', diamondInit.address)
 
-  await lock.deployed();
+  // Deploy facets and set the `facetCuts` variable
+  console.log('')
+  console.log('Deploying facets')
+  const FacetNames = [
+    'DiamondCutFacet',
+    'DiamondLoupeFacet',
+    'OwnershipFacet'
+  ]
+  // The `facetCuts` variable is the FacetCut[] that contains the functions to add during diamond deployment
+  const facetCuts = []
+  for (const FacetName of FacetNames) {
+    const Facet = await ethers.getContractFactory(FacetName)
+    const facet = await Facet.deploy()
+    await facet.deployed()
+    console.log(`${FacetName} deployed: ${facet.address}`)
+    facetCuts.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: getSelectors(facet)
+    })
+  }
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Creating a function call
+  // This call gets executed during deployment and can also be executed in upgrades
+  // It is executed with delegatecall on the DiamondInit address.
+  let functionCall = diamondInit.interface.encodeFunctionData('init')
+
+  // Setting arguments that will be used in the diamond constructor
+  const diamondArgs = {
+    owner: contractOwner.address,
+    init: diamondInit.address,
+    initCalldata: functionCall
+  }
+
+  // deploy Diamond
+  const Diamond = await ethers.getContractFactory('Diamond')
+  const diamond = await Diamond.deploy(facetCuts, diamondArgs)
+  await diamond.deployed()
+  console.log()
+  console.log('Diamond deployed:', diamond.address)
+
+  // returning the address of the diamond
+  return diamond.address
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+if (require.main === module) {
+  deployDiamond()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+exports.deployDiamond = deployDiamond
