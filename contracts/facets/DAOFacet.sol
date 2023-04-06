632ead60dcb2359f9d588e45aaf277b7d617420f
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2023-04-06 14:25:57 +0800

    init Factory and debug SignatureRecovery

--- contracts/facets/DAOFacet.sol
+++ contracts/facets/DAOFacet.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import { LibSBT } from "./SBT.sol";
 
 library  LibDAO {
