f7d49eac771bca3a61cc85693a9c8b80b31a2c5c
byeond009 <1249413181@qq.com>
2023-04-06 22:52:57 +0800

    feat: update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { config as dotEnvConfig } from "dotenv";
+import "hardhat-diamond-abi";
 dotEnvConfig();
 const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
@@ -8,12 +9,16 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.17",
+
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
+  },
+  diamondAbi: {
+    name: "NirVANA",
   },
   networks: {
     scrollAlpha: {
--- scripts/newWallet.ts
+++ scripts/newWallet.ts
@@ -1,1 +1,2 @@
 const newWallet = ethers.Wallet.createRandom();
+console.log(newWallet._signingKey().privateKey, newWallet.address);
