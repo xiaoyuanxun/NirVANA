09ae8eb355e6f98466b5f9ae118748fb0e82c423
byeond009 <1249413181@qq.com>
2023-04-06 13:27:41 +0800

    fix: fix pragma

--- contracts/facets/SignatureRecovery.sol
+++ contracts/facets/SignatureRecovery.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "../interfaces/ISignatureRecovery.sol";
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,20 +5,20 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-  // networks: {
-  //   sepolia: {
-  //     url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-  //     accounts: [PRIVATE_KEY],
-  //     gas: 2100000,
-  //     gasPrice: 8000000000,
-  //   },
-  //   scrollAlpha: {
-  //     url: "https://alpha-rpc.scroll.io/l2",
-  //     accounts: [PRIVATE_KEY],
-  //     gas: 2100000,
-  //     gasPrice: 8000000000,
-  //   },
-  // },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+    scrollAlpha: {
+      url: "https://alpha-rpc.scroll.io/l2",
+      accounts: [PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
 
 export default config;
