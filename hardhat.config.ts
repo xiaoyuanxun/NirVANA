ca288ffb7c1ec9a7bb6cba2ec5145c7c7e83b05a
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2023-04-06 13:24:07 +0800

    deploy on scroll alpha network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-  // networks: {
-  //   sepolia: {
-  //     url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-  //     accounts: [PRIVATE_KEY],
-  //     gas: 2100000,
-  //     gasPrice: 8000000000,
-  //   },
-  //   scrollAlpha: {
-  //     url: "https://alpha-rpc.scroll.io/l2",
-  //     accounts: [PRIVATE_KEY],
-  //     gas: 2100000,
-  //     gasPrice: 8000000000,
-  //   },
-  // },
+  networks: {
+    scrollAlpha: {
+      url: "https://alpha-rpc.scroll.io/l2",
+      accounts: [PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
 
 export default config;
